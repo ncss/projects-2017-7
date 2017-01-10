ea14b2f2d37b266bbd5289976c70acf178ea580c
tealkine <tealkine2@gmail.com>
2017-01-10 15:14:42 +1100

    Completed Robot.py

--- lightning/robot.py
+++ lightning/robot.py
@@ -11,10 +11,10 @@ display.show(Image.HAPPY)
 r.on()
 
 #Configures channel for communication to 12
-r.config(channel=12)
+r.config(queue=5, channel=12, data_rate=r.RATE_250KBIT)
 
 #Stops interferance from other teams with key
-key = "lightning"
+key = "l"
 
 #Functions for robot movement
 def reverse():
@@ -47,23 +47,39 @@ def turn_right():
 	pin12.write_digital(0)
 	pin8.write_digital(1)
 
+#Saves memory
+reverse_on = False
+forward_on = False
+stop_on    = False
 
 #Main loop for recieveing data
 while True:
-	#Stores radio messages into variable mess
-	mess = recv()
-	
+	#Attempts to convert recieve to string and store in mess
+	try:
+		mess = recv()
+	#If it cannot convert to string, it dumps the value and moves on
+	except ValueError:
+		r.receive_bytes()
+
 	#If mess contains a variable deal with it
 	if mess:
 		
 		#Handle different messages
-		if str(mess) == key + "f":
+		if mess == key + "f" and not forward_on:
 			forward()
-		elif str(mess) == key + "r":
+			forward_on = True
+			stop_on = False
+			reverse_on = False
+		elif mess == key + "r" and not reverse_on:
 			reverse()
-		elif str(mess) == key + "s":
+			reverse_on = True
+			forward_on = False
+			stop_on = False
+		elif mess == key + "s" and not stop_on:
 			stop()
-
+			stop_on = True
+			reverse_on = False
+			forward_on = False
 
 
 #### End Of Project ####
