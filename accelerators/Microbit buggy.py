b2cb400da449e3a5803755228e6362c054ffa047
Pytrex1 <tonylu139@gmail.com>
2017-01-10 16:46:24 +1100

    Win condition added

--- accelerators/Microbit buggy.py
+++ accelerators/Microbit buggy.py
@@ -1,6 +1,6 @@
 from microbit import *
 import radio
-
+win = False
 radio.on()
 radio.config(channel=95,
 address = 0x75626974)
@@ -17,13 +17,19 @@ def stop():
     pin12.write_digital(0)
     pin8.write_digital(0)
 
-    
+start = running_time()
 while True:
     message = radio.receive()
     if message:
         forward()
         sleep(500)
         stop()
-
-  
-    
+    if win == False:
+        if pin1.read_analog() < 10 or pin2.read_analog() < 10:
+            end = running_time()
+            timer = int((end - start)/1000)
+            radio.send('I won!')
+            display.scroll('You won! You took ' + str(timer) + 's', wait = False)
+            win = True
+    if button_b.was_pressed():
+        win = False
