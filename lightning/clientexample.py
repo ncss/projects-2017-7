6e577146b558b37699bece953e210cb963234f79
Tim Handley <timhandley101@gmail.com>
2017-01-10 15:09:01 +1100

    Finalised sending script

--- lightning/clientexample.py
+++ lightning/clientexample.py
@@ -6,11 +6,12 @@ import radio
 #enable radio interface
 radio.on()
 
+
 #Set up radio channel
-radio.config(channel=12)
+radio.config(channel=12, data_rate=radio.RATE_250KBIT)
 
 #Key for messages
-key = "lightning"
+key = "l"
 
 
 #Permanent loop
@@ -18,7 +19,7 @@ while True:
 	#Both buttons = Stop, A = forward, B = reverse
 	if button_a.is_pressed() and button_b.is_pressed():
 		radio.send(key + "s")
-	elif button_a.is_pressed():
+	elif button_a.was_pressed():
 		radio.send(key + "f")
-	elif button_b.is_pressed():
+	elif button_b.was_pressed():
 		radio.send(key + "r")
