4e746e4f7a3ebc3b440117421823fddc9f91de44
Ming <luy17@student.barker.nsw.edu.au>
2017-01-10 14:48:22 +1100

    R2D2 MVP: button complete

--- R2D2/button.py
+++ R2D2/button.py
@@ -1,14 +1,19 @@
 from microbit import *
+import radio
+radio.on()
 radio.config(channel=58)
-
+button_type = 'left'
 while True:
-    # The button is context free, and can be configured with the a and b buttons.
+    # The big button is context free, and can be configured with the a and b buttons.
+    if button_type == 'left':
+        display.show(Image.ARROW_W)
+    if button_type == 'right':
+        display.show(Image.ARROW_E)
     if button_a.is_pressed():
         button_type = 'left'
-        display.show(ARROW_W)
     if button_b.is_pressed():
         button_type = 'right'
-        display.show(ARROW_E)
     if pin8.read_digital() == 1:
         radio.send(button_type)
+        display.show(Image.SQUARE_SMALL)
     sleep(5)
