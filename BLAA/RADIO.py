e67e7a9968fe78b71749b5920dcf825af2f35222
dark <44355@stpeters.qld.edu.au>
2017-01-10 14:45:16 +1100

    updated Radio and Score
    score for jumping/crouching/sliding under/over obstacle

--- BLAA/RADIO.py
+++ BLAA/RADIO.py
@@ -4,9 +4,34 @@ radio.on()
 while True:
   if crouch == 1:
     radio.send("crouch")
-  if crouch == 1:
+  if slide == 1:
     radio.send("slide")
-  if crouch == 1:
+  if jump == 1:
     radio.send("jump")
-  if crouch == 1:
+  if stand == 1:
     radio.send("stand")
+    
+from microbit import *
+import radio
+
+radio.on()
+while True:
+  score = 0
+  message = radio.receive()
+  if message == "crouch":
+      sleep(10)
+      score = score + 1
+      sleep(10)
+
+  if message == "slide":
+      sleep(10)
+      score = score + 1
+      sleep(10)
+
+  if message == "jump":
+      sleep(10)
+      score = score + 1
+      sleep(10)
+
+  if message == "stand":
+      sleep(10)   
