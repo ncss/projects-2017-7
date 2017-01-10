65a8261feeeebc8716363922d536893ff3339b04
Pytrex1 <tonylu139@gmail.com>
2017-01-10 15:25:33 +1100

    Radio w/ microbit

---
+++ accelerators/Microbit buggy.py
@@ -0,0 +1,30 @@
+from microbit import *
+import radio
+
+radio.on()
+radio.config(channel=95,
+address = 0x75626974)
+
+def forward():
+    pin0.write_digital(1)
+    pin16.write_digital(0)
+    pin12.write_digital(0)
+    pin8.write_digital(1)
+        
+def stop():
+    pin0.write_digital(0)
+    pin16.write_digital(0)
+    pin12.write_digital(0)
+    pin8.write_digital(0)
+
+    
+while True:
+    message = radio.receive()
+    if message:
+        print('test')
+        forward()
+        sleep(500)
+        stop()
+
+  
+    
