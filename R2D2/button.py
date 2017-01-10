bcddc878b3a4e973cd9f30b03bfe605c7cb46c43
Ming <luy17@student.barker.nsw.edu.au>
2017-01-10 14:22:52 +1100

    MVP button implementation

--- R2D2/button.py
+++ R2D2/button.py
@@ -1,9 +1,14 @@
 from microbit import *
-pin8.read_digital()
-pin8.PULL_DOWN
+radio.config(channel=58)
 
 while True:
+    # The button is context free, and can be configured with the a and b buttons.
+    if button_a.is_pressed():
+        button_type = 'left'
+        display.show(ARROW_W)
+    if button_b.is_pressed():
+        button_type = 'right'
+        display.show(ARROW_E)
     if pin8.read_digital() == 1:
-        display.show(Image.ANGRY)
-    else:
-        display.clear()
+        radio.send(button_type)
+    sleep(5)
