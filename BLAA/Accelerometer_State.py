af35708f2e33db181b6e1ddfd7d52d6cd9d573e1
lizcXD <ielizabethlc@gmail.com>
2017-01-10 15:35:26 +1100

    Adjusted Y Range and sleep
    All working, however reliability of the crouch/jump controls needing
    improvement. Radio should be working. Included feedback for
    calibrations.

--- BLAA/Accelerometer_State.py
+++ BLAA/Accelerometer_State.py
@@ -9,18 +9,21 @@ initial_acceleration = accelerometer.get_y()
 while True:
     y_axis = accelerometer.get_y() - initial_acceleration
     
-    if button_a.was_pressed() and button_b.was_pressed():
+    if button_a.is_pressed() and button_b.is_pressed():
         initial_acceleration = accelerometer.get_y()
+        display.show(Image.HEART)
+        sleep(500)
+        display.clear()
     
     if y_axis > 600:
-        sleep(150)
+        sleep(200)
         display.show(Image.ARROW_N, wait=False)
         radio.send("jump")
         sleep(1000)
         display.clear()
     
-    if y_axis < -700:
-        sleep(150)
+    if y_axis < -600:
+        sleep(400)
         display.show(Image.ARROW_S, wait=False)
         radio.send("crouch")
         sleep(1000)
