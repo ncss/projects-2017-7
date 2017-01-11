d02bee7ca362ddf56cdb02c3f18945b7c3083b76
anon1234512 <luy17@student.barker.nsw.edu.au>
2017-01-11 12:03:11 +1100

    Changed button display behaviour

--- R2D2/button.py
+++ R2D2/button.py
@@ -5,36 +5,61 @@ radio.config(channel=58)
 button_type = 'left'
 under_time = True
 initial = True
-right_pressed = False
+
+left_arrow = Image(
+"00000:"
+"01000:"
+"11111:"
+"01000:"
+"00000:"
+)
+
+right_arrow = Image(
+"00000:"
+"00010:"
+"11111:"
+"00010:"
+"00000:"
+)
+
+button_time = 0
+button_lookup = {}
+button_lookup['left'] = left_arrow
+button_lookup['right'] = right_arrow
+button_type = 'left'
+
 
 
 while True:
     # The big button is context free, and can be configured with the a and b buttons.
-    if button_type == 'left':
-        display.show(Image.ARROW_W)
-    if button_type == 'right':
-        display.show(Image.ARROW_E)
     if button_a.is_pressed():
         button_type = 'left'
     if button_b.is_pressed():
         button_type = 'right'
     if under_time:
         if pin8.read_digital() == 1:
+            button_time = running_time()
             radio.send(button_type)
-            display.show(Image.SQUARE_SMALL)
+            display.show(button_lookup[button_type]*9)
             if initial:
                 start_time = running_time()
                 initial = False
             else:
                 if running_time() - start_time < 300:
                     continue
-                else: under_time = False
+                else: 
+                    under_time = False
             #radio.send("music")
             sleep(50)
         else:
             initial = True
     else:
-        sleep(3000)
+        print("ok")
+        display.show(button_lookup[button_type] * 4)
+        sleep(1500)
         under_time = True
         initial = True
+        
+    if button_time + 100 < running_time():
+        display.show(button_lookup[button_type] * 4)
     
